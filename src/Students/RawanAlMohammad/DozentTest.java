fe680dc48e7431f91471dd7cde6b85a6de39d7ab
rwan mohammad <rwan4498@outlook.com>
2022-11-25 00:19:27 +0100

    Dozent

---
+++ src/Students/RawanAlMohammad/DozentTest.java
@@ -0,0 +1,11 @@
+package Students.RawanAlMohammad;
+
+public class DozentTest {
+    public static void main(String[] args) {
+        Dozent diedozentin  = new Dozent( "Jeen","Musik",'F');
+        Dozent derDekan  = new Dozent( "Ali","Mathe",'M');
+        System.out.println("Name:" + derDekan.getName() + ", "+ "lehrgebiet:"+ derDekan.getLehrgebiet() +", "+"geschlecht:" +derDekan.getGeschlecht());
+        diedozentin.setLehrgebiet("Englisch");
+        System.out.println(diedozentin.getLehrgebiet());
+    }
+}
